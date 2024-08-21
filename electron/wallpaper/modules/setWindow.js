9e2e5377543c276b39209b065fe9a241e2b26ae9
jares <953533263@qq.com>
2024-08-21 23:39:39 +0800

    修改设置界面

--- electron/wallpaper/modules/setWindow.js
+++ electron/wallpaper/modules/setWindow.js
@@ -1,10 +1,19 @@
+/*
+ * @Author: jares
+ * @Date: 2024-08-06 22:13:22
+ * @LastEditors: jares
+ * @LastEditTime: 2024-08-21 23:33:24
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by jares, All Rights Reserved. 
+ */
 const path = require('path')
 const { app, BrowserWindow, ipcMain, shell } = require('electron')
 const setWindow = () => {
 	const child = new BrowserWindow({
 		// parent: mainWindow,
-		modal: true,
-		show: true,
+		// modal: true,
+		// show: true,
 		width: 800,
 		height: 600,
 		icon: path.join(__dirname, '../static/images/icon.png'),
@@ -12,7 +21,7 @@ const setWindow = () => {
 			preload: path.join(__dirname, '../preload.js')
 		},
 		frame: false, // 无边框窗口
-		transparent: false // 窗口透明
+		transparent: true // 窗口透明
 	})
 	child.loadFile(path.join(__dirname, '../html/set.html'))
 	child.webContents.openDevTools()
